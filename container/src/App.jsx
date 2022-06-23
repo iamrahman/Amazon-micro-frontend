3e0a63b4ae66e4713344c0cdae2d0236af3bf899
Inamur Rahman <irehman@rivian.com>
2022-06-23 15:01:31 +0530

    mfe header

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "./components/Header";
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <div>Name: container</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
+   <Header/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
